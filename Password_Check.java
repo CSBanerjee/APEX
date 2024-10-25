fed3c24ccaf7c52ea360faec0c11064b4694c09d
Chandra Shekhar Banerjee <55144427+CSBanerjee@users.noreply.github.com>
2024-10-26 01:51:07 +0530

    Add files via upload

--- Password_Check.java
+++ Password_Check.java
@@ -10,7 +10,7 @@ public class BasicClassInApex {
         system.debug(finalAnswer);
         finalAnswer = handlePasswordAttempts('thirdattemt');
         system.debug(finalAnswer);
-        finalAnswer = handlePasswordAttempts('thirdattemt');
+        finalAnswer = handlePasswordAttempts('My_Salesforce_Password');
         system.debug(finalAnswer);
     }
     
@@ -25,6 +25,44 @@ public class BasicClassInApex {
         else{
             return 'success';
         }        
+    }
+    
+    public static integer checkForMakeSense(List<String> statements){
+        integer makesSense = 0;
+        for(string statement:statements ){
+            if(statement.containsIgnoreCase('Make Sense')){
+                makesSense++;
+            }
+        }
+        return makesSense;
+    }
+    
+    public static void checkHighestLowest(Map<String,Integer> myReportCard){
+        integer maxMarks = 0;
+        integer minMarks = 0;
+        /*Map<String,Integer> myReportCard = new Map<String,Integer>();
+        myReportCard.put('Physics',88);
+        myReportCard.put('Chemistry',66);
+        myReportCard.put('Biology',77);
+        myReportCard.put('Math',98);*/
+        for(String subject:myReportCard.keyset()){
+            if(minMarks ==0){
+            minMarks = myReportCard.get(subject);    
+            }
+            integer subjectMark = myReportCard.get(subject);
+            if(maxMarks<subjectMark){
+                maxMarks = subjectMark;
+            }
+            if(minMarks>subjectMark){
+                minMarks = subjectMark;
+            }
+
+            
+        }
+        system.debug(maxMarks);
+        system.debug(minMarks);
+            
+        
     }
 
 }
